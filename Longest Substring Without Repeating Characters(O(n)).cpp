1538928b182318a3b7cf2d92745ad9f67e936134
jijiwaiwai2016 <1020298415@qq.com>
2015-04-29 20:05:11 +0800

    输出字符串中最长子串的长度

---
+++ Longest Substring Without Repeating Characters(O(n)).cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Longest Substring Without Repeating Characters(O(n))
+//
+//  Created by 唧唧歪歪 on 15/4/29.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char,int> map;
+        int i;
+        int count=0;
+        int maxlen=0;
+        //count表示当前考虑的子串的长度；
+        for(i=0;i<s.size();i++)
+        {
+            count++;
+            if(map.count(s[i])!=0&&map[s[i]]>i-count)
+            {
+                count=i-map[s[i]];
+            }
+            map[s[i]]=i;
+            if(maxlen<count)
+            {
+                maxlen=count;
+            }
+        }
+        return maxlen;
+    }
+};
+int main()
+{
+    Solution ss;
+    cout<<"请输入字符串s"<<endl;
+    string str;
+    while(cin>>str)
+    {
+        cout<<"最长不重复字串为:"<<ss.lengthOfLongestSubstring(str)<<endl;
+        cout<<"请输入字符串s"<<endl;
+    }
+}
