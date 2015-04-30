b3c15e4472297ee4d0016c5f8693a29c12a547bc
jijiwaiwai2016 <1020298415@qq.com>
2015-04-30 09:33:36 +0800

    动态规划切割钢条法

---
+++ 朴素递归求解切割钢条法.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  Cut_Rod
+//
+//  Created by 唧唧歪歪 on 15/4/27.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+//利用朴素递归求解钢条切割的最大收益；
+int CutRod(vector<int> p,int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    int q=-99999;//表示最大收益；
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        int temp=p[i]+CutRod(p,n-i);
+        if(q<temp)
+        {
+            q=temp;
+        }
+    }
+    return q;
+}
+
+//主函数；
+int main()
+{
+    vector<int> p;
+    p.push_back(0);
+    int size;
+    cout<<"请输入数组的数量:"<<endl;
+    cin>>size;
+    cout<<"请输入"<<size<<"个数据:"<<endl;
+    int i;//表示临时变量；
+    int temp;
+    for(i=0;i<size;i++)
+    {
+        cin>>temp;
+        p.push_back(temp);
+    }
+    while(1)
+    {
+        cout<<"请输入需要计算的n的值:"<<endl;
+        int n;
+        cin>>n;
+        cout<<"最大收益为:"<<CutRod(p,n)<<endl;
+    }
+}
---
+++ 自底向上动态规划切割钢条法.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  自底向上动态规划算法
+//
+//  Created by 唧唧歪歪 on 15/4/27.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+//自底向上动态规划求解切割钢条；
+int Bottom_Up_Cut_Rod(vector<int> p,int n)
+{
+    vector<int> r;
+    int i,j;
+    for(i=0;i<=n;i++)
+    {
+        r.push_back(-99999);
+    }
+    r[0]=0;
+    
+    int q;
+    int temp;//临时变量；
+    for(j=1;j<=n;j++)
+    {
+        q=-99999;
+        for(i=1;i<=j;i++)
+        {
+            temp=p[i]+r[j-i];
+            if(q<temp)
+            {
+                q=temp;
+            }
+        }
+        r[j]=q;
+    }
+    return r[n];
+}
+//主函数；
+int main()
+{
+    vector<int> p;
+    p.push_back(0);
+    int size;
+    cout<<"请输入数组的大小:"<<endl;
+    cin>>size;
+    int i;
+    int temp;
+    cout<<"请输入"<<temp<<"个数据:"<<endl;
+    for(i=0;i<size;i++)
+    {
+        cin>>temp;
+        p.push_back(temp);
+    }
+    int n;
+    cout<<"请输入要计算的钢条长度:"<<endl;
+    cin>>n;
+    while(1)
+    {
+        cout<<"最大利益为:"<<Bottom_Up_Cut_Rod(p,n)<<endl;
+        cout<<endl;
+        cout<<"请输入要计算的钢条长度:"<<endl;
+        cin>>n;
+    }
+}
