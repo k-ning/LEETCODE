041743c97932391c4d9a8cc1b08e348d9345a0fc
jijiwaiwai2016 <1020298415@qq.com>
2015-04-30 11:17:55 +0800

    LeetCode NO.4
    直接使用归并排序的思想，对两个已经有序的数组排序，然后直接返回中位数。

---
+++ Median of Two Sorted Arrays.cpp
@@ -0,0 +1,96 @@
+//
+//  main.cpp
+//  Median of Two Sorted Arrays
+//
+//  Created by 唧唧歪歪 on 15/4/29.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    //查找两个已经排序好数组的中间值；
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        int i;//表示临时变量；
+        int l1,l2;//表示两个数组的指针；
+        l1=0;
+        l2=0;
+        vector<int> nums3;//表示临时变量；
+        int size=(int)nums1.size()+(int)nums2.size();
+        
+        for(i=0;i<size;i++)
+        {
+            //条件；
+            if(l1<(int)nums1.size()&&l2<(int)nums2.size())
+            {
+                if(nums1[l1]>nums2[l2])
+                {
+                    nums3.push_back(nums2[l2]);
+                    l2++;
+                }
+                else if(nums1[l1]<=nums2[l2])
+                {
+                    nums3.push_back(nums1[l1]);
+                    l1++;
+                }
+            }
+        }
+        while(l2<(int)nums2.size())
+        {
+            nums3.push_back(nums2[l2++]);
+        }
+        while(l1<(int)nums1.size())
+        {
+            nums3.push_back(nums1[l1++]);
+        }
+        /*
+        cout<<"合并完成的数组为:"<<endl;
+        for(i=0;i<(int)nums3.size();i++)
+        {
+            cout<<nums3[i]<<" ";
+        }
+        cout<<endl;
+        */
+        double tt;
+        if((size%2)!=0)
+        {
+            tt=nums3[(size-1)/2];
+            return tt;
+        }
+        else
+        {
+            tt=(nums3[(size-1)/2]+nums3[size/2])/2.0000;
+            return tt;
+        }
+    }
+};
+
+//主函数；
+int main()
+{
+    vector<int> nums1,nums2;//表示两个数组；
+    int size1,size2;//表示两个数组的大小；
+    cout<<"请输入两个数组的大小:"<<endl;
+    cin>>size1>>size2;
+    cout<<"请输入第一个数组的"<<size1<<"个数据:"<<endl;
+    int i;
+    int temp;
+    for(i=0;i<size1;i++)
+    {
+        cin>>temp;
+        nums1.push_back(temp);
+    }
+    cout<<"请输入第二个数组的"<<size2<<"个数据:"<<endl;
+    for(i=0;i<size2;i++)
+    {
+        cin>>temp;
+        nums2.push_back(temp);
+    }
+    Solution ss;
+    cout<<"两个数组的中值为:"<<ss.findMedianSortedArrays(nums1,nums2)<<endl;
+}
