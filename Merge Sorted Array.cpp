6648e23121fa45065dc46aeed7c461ad72df83fc
jijiwaiwai2016 <1020298415@qq.com>
2015-05-09 20:29:13 +0800

    LeetCode NO.88
    7ms刷过～～为自己撒花～～～

---
+++ Merge Sorted Array.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  Merge Sorted Array
+//
+//  Created by 唧唧歪歪 on 15/5/9.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        if(n==0)
+        {
+            return ;
+        }
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        
+        while(i>=0&&j>=0)
+        {
+            if(nums1[i]>nums2[j])
+            {
+                nums1[k--]=nums1[i--];
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                nums1[k--]=nums2[j--];
+            }
+            else if(nums1[i]==nums2[j])
+            {
+                nums1[k--]=nums1[i--];
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(i!=-1)
+        {
+            nums1[k--]=nums1[i--];
+        }
+        while(j!=-1)
+        {
+            nums1[k--]=nums2[j--];
+        }
+    }
+};
+
+int main()
+{
+
+}
