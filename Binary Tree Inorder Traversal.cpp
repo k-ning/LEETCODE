c106b2f4db6f79d7e533796d15961bf09b8a81e7
jijiwaiwai2016 <1020298415@qq.com>
2015-05-11 12:55:58 +0800

    LeetCode NO.94
    非递归中序遍历二叉树~~3ms~~刷过~~

---
+++ Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Binary Tree Inorder Traversal
+//
+//  Created by 唧唧歪歪 on 15/5/11.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+//定义二叉树节点；
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        vector<int> v;
+        stack<TreeNode *> s;
+        TreeNode * tree=root;
+        while(tree!=NULL||s.empty()!=true)
+        {
+            if(tree!=NULL)
+            {
+                s.push(tree);
+                tree=tree->left;
+            }
+            else
+            {
+                tree=s.top();
+                v.push_back(tree->val);
+                s.pop();
+                tree=tree->right;
+            }
+        }
+        return v;
+    }
+};
+int main()
+{
+
+}
