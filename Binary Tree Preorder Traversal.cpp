213a27ad06a26b411cc4c14e5bcb186691e22511
jijiwaiwai2016 <1020298415@qq.com>
2015-05-11 11:27:59 +0800

    LeetCode NO.144
    3ms~~刷过~~棒棒哒~~

---
+++ Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  Binary Tree Preorder Traversal
+//
+//  Created by 唧唧歪歪 on 15/5/11.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+//定义二叉树的数据结构；
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        vector<int> v;
+        if(root==NULL)
+        {
+            return v;
+        }
+        
+        TreeNode * t=root;
+        stack<TreeNode *> s;
+        s.push(t);
+        while(s.empty()!=true)
+        {
+            t=s.top();
+            v.push_back(t->val);
+            s.pop();
+            
+            if(t->right!=NULL)
+            {
+                s.push(t->right);
+            }
+            if(t->left!=NULL)
+            {
+                s.push(t->left);
+            }
+        }
+        return v;
+    }
+};
+int main()
+{
+}
