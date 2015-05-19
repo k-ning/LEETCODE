63d4483c344bffd3eb9fed4ccd1855dbc7614c75
jijiwaiwai2016 <1020298415@qq.com>
2015-05-19 08:53:54 +0800

    LeetCode NO.142
    判断链表是否含有环，同时找到环的第一个结点~~~

---
+++ Linked List Cycle II.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  Linked List Cycle II
+//
+//  Created by 唧唧歪歪 on 15/5/19.
+//  Copyright (c) 2015年 唧唧歪歪. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        if(head==NULL)
+            return NULL;
+        ListNode * slow=head;
+        ListNode * quick=head;
+        
+        while(quick!=NULL&&quick->next!=NULL)
+        {
+            slow=slow->next;
+            quick=quick->next->next;
+            if(quick==slow)
+            {
+                break;
+            }
+        }
+        if(quick==NULL||quick->next==NULL)
+        {
+            return NULL;
+        }
+        slow=head;
+        while(slow!=quick)
+        {
+            slow=slow->next;
+            quick=quick->next;
+        }
+        return slow;
+    }
+};
+int main()
+{
+
+}
