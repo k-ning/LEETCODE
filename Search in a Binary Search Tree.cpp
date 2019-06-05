a19701b514c7881efd92604e9a7389756f3d70e1
jijiwaiwai2016 <1020298415@qq.com>
2019-06-06 07:13:18 +0800

    LeetCode No.700 Search in a Binary Search Tree

---
+++ Search in a Binary Search Tree.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), rigth(NULL) {}
+};
+TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == NULL) return root;
+        
+        if (root->val == val)
+        	return root;
+        if (root->val > val) 
+        	return searchBST(root->left, val);
+        else
+        	return searchBST(root->right, val);
+}
