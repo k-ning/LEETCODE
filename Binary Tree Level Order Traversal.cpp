6f3f4ba766c2870752b1c63325959c5540d9c5ba
jijiwaiwai2016 <1020298415@qq.com>
2019-06-06 23:04:34 +0800

    Add another version of Binary Tree Level Order Traversal.

--- Binary Tree Level Order Traversal.cpp
+++ Binary Tree Level Order Traversal.cpp
@@ -20,38 +20,77 @@ struct TreeNode
 };
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> v;
-        if (root == NULL)
-            return v;
+        vector<vector<int>> levelOrder(TreeNode* root) {
+
+                vector<vector<int>> v;
+                if (root == NULL)
+                        return v;
         
-        TreeNode * tree = root;
-        queue<TreeNode *> q;
-        q.push(tree);
+                TreeNode* tree = root;
+                queue<TreeNode*> q;
+                q.push(tree);
         
-        vector<int> temp;
-        int size = (int)q.size();
+                vector<int> temp;
+                int size = (int)q.size();
         
-        while (q.empty() != true) {
-                while (size != 0) {
-                        tree = q.front();
-                        q.pop();
-                        temp.push_back(tree->val);
+                while (q.empty() != true) {
+                        while (size != 0) {
+                                tree = q.front();
+                                q.pop();
+                                temp.push_back(tree->val);
                 
-                        if (tree->left != NULL) {
-                                q.push(tree->left);
+                                if (tree->left != NULL) {
+                                        q.push(tree->left);
+                                }
+                                if (tree->right != NULL) {
+                                        q.push(tree->right);
+                                }
+                                size--;
+                        }
+                        size = (int)q.size();
+                        v.push_back(temp);
+                        temp.clear();
                 }
-                if (tree->right != NULL) {
-                    q.push(tree->right);
-                }
-                size--;
-            }
-            size = (int)q.size();
-            v.push_back(temp);
-            temp.clear();
+                return v;
         }
-        return v;
-    }
+        
+        vector<vector<int>> levelOrder_new(TreeNode* root) {
+
+                vector<vector<int>> binaryTree;
+                vector<int> binaryTreeTemp;
+                queue<TreeNode*> treeNodeQueue;
+                TreeNode* node = root;
+                int size = 0;
+        
+                if (root == NULL)
+                        return binaryTree;
+        
+                treeNodeQueue.push(node);
+                binaryTreeTemp.push_back(node->val);
+        
+                while (treeNodeQueue.empty() != true) {
+                        binaryTree.push_back(binaryTreeTemp);
+                        binaryTreeTemp.clear();
+                        size = treeNodeQueue.size();
+            
+                        while (size != 0) {
+                                node = treeNodeQueue.front();
+                                treeNodeQueue.pop();
+                
+                                if (node->left != NULL) {
+                                        treeNodeQueue.push(node->left);
+                                        binaryTreeTemp.push_back(node->left->val);
+                                }
+                
+                                if (node->right != NULL) {
+                                        treeNodeQueue.push(node->right);
+                                        binaryTreeTemp.push_back(node->right->val);
+                                }
+                                size--;
+                        }
+                }
+                return binaryTree;
+        }
 };
 int main() {
 
