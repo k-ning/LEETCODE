8a3b35c18de7211e09ef9db1254d5ebdcbb69d5b
jijiwaiwai2016 <1020298415@qq.com>
2019-06-08 14:49:27 +0800

    Minor changes on the code format.

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -12,44 +12,34 @@ using namespace std;
 class Solution
 {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
-    {
-        if(n==0)
-        {
-            return ;
-        }
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        while(i>=0&&j>=0)
-        {
-            if(nums1[i]>nums2[j])
-            {
-                nums1[k--]=nums1[i--];
-            }
-            else if(nums1[i]<nums2[j])
-            {
-                nums1[k--]=nums2[j--];
-            }
-            else if(nums1[i]==nums2[j])
-            {
-                nums1[k--]=nums1[i--];
-                nums1[k--]=nums2[j--];
-            }
+                if (n == 0) {
+                        return;
+                }
+                int i = m - 1;
+                int j = n - 1;
+                int k = m + n - 1;
+        
+                while (i >= 0 && j >= 0) {
+                        if (nums1[i] > nums2[j]) {
+                                nums1[k--] = nums1[i--];
+                        } else if (nums1[i] < nums2[j]) {
+                                nums1[k--] = nums2[j--];
+                        } else if (nums1[i] == nums2[j]) {
+                                nums1[k--] = nums1[i--];
+                                nums1[k--] = nums2[j--];
+                        }
+                }
+                while (i != -1) {
+                        nums1[k--] = nums1[i--];
+                }
+                while (j != -1) {
+                        nums1[k--] = nums2[j--];
+                }
         }
-        while(i!=-1)
-        {
-            nums1[k--]=nums1[i--];
-        }
-        while(j!=-1)
-        {
-            nums1[k--]=nums2[j--];
-        }
-    }
 };
 
-int main()
-{
+int main() {
 
 }
