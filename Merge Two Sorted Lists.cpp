2c3ba8ec63c10b9ab318ecf26595326500192e13
jijiwaiwai2016 <1020298415@qq.com>
2019-06-08 18:28:18 +0800

    LeetCode No.21 Merge Two Sorted Lists.

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 using namespace std;
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+	if (l1 == NULL) return l2;
+	if (l2 == NULL) return l1;
+        
+	ListNode* head = new ListNode(0);
+	ListNode* temp = head;
+        
+	while (l1 && l2) {
+		if (l1->val > l2->val) {
+			temp->next = l2;
+			temp = temp->next;
+
+			l2 = l2->next;
+		} else if (l1->val < l2->val) {
+			temp->next = l1;
+			temp = temp->next;
+
+			l1 = l1->next;
+		} else {
+			temp->next = l1;
+			temp = l1;
+			l1 = l1->next;
+
+			temp->next = l2;
+			temp = temp->next;
+			l2 = l2->next;
+		}
+	}
+	//善用三目运算符;
+	temp->next = (l1 == NULL) ? l2 : l1;
+	return head->next;
+}
