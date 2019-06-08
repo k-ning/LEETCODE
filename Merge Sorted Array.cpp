4320317117b05702188e99d5245bf323cb122123
jijiwaiwai2016 <1020298415@qq.com>
2019-06-08 16:24:47 +0800

    Update version of Merge Sorted Array

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -9,6 +9,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 class Solution
 {
 public:
@@ -20,8 +21,13 @@ public:
                 int i = m - 1;
                 int j = n - 1;
                 int k = m + n - 1;
-        
+                
+                /* 原址排序. */
                 while (i >= 0 && j >= 0) {
+
+                        nums1[k--] = nums1[i] >= nums2[j] ? nums1[i--] : nums2[j--];
+                        /* can be replaced. */
+                        /*
                         if (nums1[i] > nums2[j]) {
                                 nums1[k--] = nums1[i--];
                         } else if (nums1[i] < nums2[j]) {
@@ -30,10 +36,14 @@ public:
                                 nums1[k--] = nums1[i--];
                                 nums1[k--] = nums2[j--];
                         }
+                        */
                 }
+                /* can be omitted. */
+                /*
                 while (i != -1) {
                         nums1[k--] = nums1[i--];
                 }
+                */
                 while (j != -1) {
                         nums1[k--] = nums2[j--];
                 }
