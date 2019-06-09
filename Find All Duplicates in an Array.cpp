eaaa57a5ae7d46c326d8f9700ccc47e468145ca0
jijiwaiwai2016 <1020298415@qq.com>
2019-06-09 11:26:27 +0800

    Add another method to find all duplicates in an array.

--- Find All Duplicates in an Array.cpp
+++ Find All Duplicates in an Array.cpp
@@ -25,3 +25,26 @@ vector<int> findDuplicates_1(vector<int>& nums) {
 	return temp;
 }
 
+// Method Two.
+// A great method which uses the original array to sort.
+// Using the hash searching method.
+vector<int> findDuplicates(vector<int>& nums) {
+	int num = 0;
+	vector<int> temp;
+
+	if (nums.size() == 0) {
+		return temp;
+	}
+	
+	for (int i = 0; i < nums.size(); i++) {
+		num = abs(nums[i]);
+
+		if (nums[num - 1] < 0) {
+			temp.push_back(num);
+		} else if (nums[num - 1] > 0) {
+			nums[num - 1] *= -1;
+		}
+	}
+	return temp;
+}
+
