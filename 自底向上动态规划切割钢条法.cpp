6f69ffc1a8b0e62316a96464c29056b55b8b983f
Juan <18621563883@163.com>
2020-08-30 00:22:22 +0800

    Update 自底向上动态规划切割钢条法.cpp

--- 自底向上动态规划切割钢条法.cpp
+++ 自底向上动态规划切割钢条法.cpp
@@ -9,31 +9,31 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 //自底向上动态规划求解切割钢条；
-int Bottom_Up_Cut_Rod(vector<int> p,int n)
-{
+int Bottom_Up_Cut_Rod(vector<int> p, int n) {
+
     vector<int> r;
-    int i,j;
-    for(i=0;i<=n;i++)
-    {
+    int i, j;
+
+    for (i = 0; i <= n; i++) {
+
         r.push_back(-99999);
     }
-    r[0]=0;
+    r[0] = 0;
     
     int q;
     int temp;//临时变量；
-    for(j=1;j<=n;j++)
-    {
-        q=-99999;
-        for(i=1;i<=j;i++)
-        {
-            temp=p[i]+r[j-i];
-            if(q<temp)
-            {
-                q=temp;
+    for (j = 1; j <= n; j++) {
+        q = -99999;
+        for (i = 1; i <= j; i++) {
+            temp = p[i] + r[j - i];
+
+            if (q < temp) {
+                q = temp;
             }
         }
-        r[j]=q;
+        r[j] = q;
     }
     return r[n];
 }
@@ -43,24 +43,26 @@ int main()
     vector<int> p;
     p.push_back(0);
     int size;
-    cout<<"请输入数组的大小:"<<endl;
-    cin>>size;
+    cout << "请输入数组的大小:" << endl;
+    cin >> size;
+
     int i;
     int temp;
-    cout<<"请输入"<<temp<<"个数据:"<<endl;
-    for(i=0;i<size;i++)
-    {
-        cin>>temp;
+    cout << "请输入" << temp << "个数据:" << endl;
+
+    for (i = 0; i < size; i++) {
+        cin >> temp;
         p.push_back(temp);
     }
+
     int n;
-    cout<<"请输入要计算的钢条长度:"<<endl;
-    cin>>n;
-    while(1)
-    {
-        cout<<"最大利益为:"<<Bottom_Up_Cut_Rod(p,n)<<endl;
-        cout<<endl;
-        cout<<"请输入要计算的钢条长度:"<<endl;
-        cin>>n;
+    cout << "请输入要计算的钢条长度:" << endl;
+    cin >> n;
+
+    while (1) {
+        cout << "最大利益为:" << Bottom_Up_Cut_Rod(p, n) << endl;
+        cout << endl;
+        cout << "请输入要计算的钢条长度:" << endl;
+        cin >> n;
     }
 }
